49135951a73d0aff5f96194e61a85ead3f556cfd
Tina Müller <cpan2@tinita.de>
2018-04-21 19:34:12 +0200

    Keep backwards compatibility by returning JSON::PP::Boolean objects

--- lib/bool.pm
+++ lib/bool.pm
@@ -2,39 +2,54 @@ use strict;
 use warnings;
 package bool;
 
-use Scalar::Util ();
-
-use strict;
-use overload (
-    "0+"     => sub { ${$_[0]} },
-    "++"     => sub { $_[0] = ${$_[0]} + 1 },
-    "--"     => sub { $_[0] = ${$_[0]} - 1 },
-    fallback => 1,
-);
-
 use Exporter ();
 BEGIN { @bool::ISA = ('Exporter') }
 
-our ($TRUE, $FALSE);
 our @EXPORT_OK = qw/ true false /;
+require JSON::PP;
+@JSON::PP::Boolean::ISA = qw/ bool /;
+
+use overload (
+    "0+"     => \&_stringify,
+    "++"     => \&_plus,
+    "--"     => \&_minus,
+    fallback => 1,
+);
 
 sub new {
     my ($class, $num) = @_;
+    $num = $num ? 1 : 0;
     return bless \$num, $class;
 }
 
-$TRUE = bool->new(1);
-$FALSE = bool->new(0);
+my $TRUE = do { bless \(my $dummy = 1), "JSON::PP::Boolean" };
+my $FALSE = do { bless \(my $dummy = 0), "JSON::PP::Boolean" };
 
 sub true { $TRUE }
 sub false { $FALSE }
 
 sub is_bool {
-    Scalar::Util::blessed($_[0]) and $_[0]->isa("bool");
+    JSON::PP::is_bool(@_);
 }
 
 sub complement {
     $_[0] ? $FALSE : $TRUE
+}
+
+sub _stringify {
+    ${ $_[0] }
+}
+
+sub _plus {
+    $_[0] = ${ $_[0] } + 1
+}
+
+sub _minus {
+    $_[0] = ${ $_[0] } - 1
+}
+
+sub perl {
+    ${ $_[0] } ? 1 : ''
 }
 
 1;
---
+++ t/10.bool.t
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use Test::More tests => 11;
+
+use bool;
+use Data::Dumper;
+
+my $true = bool::true;
+my $false = bool::false;
+
+ok(bool::is_bool($true), "is_bool(\$true)");
+ok(bool::is_bool($false), "is_bool(\$false)");
+
+ok($true, "\$true is true");
+ok(!$false, "\$false is false");
+
+ok(!$true == $false, "!\$true == \$false");
+ok(!$false == $true, "!\$false == \$true");
+
+cmp_ok(++$false, '==', 1, "++\$bool == 1");
+$false = bool::false;
+
+cmp_ok("$true", 'eq', 1, "\$true eq 1");
+cmp_ok("$false", 'eq', 0, "\$false eq 0");
+
+cmp_ok($true->perl, 'eq', 1, "\$true->perl eq 1");
+cmp_ok($false->perl, 'eq', '', "\$false->perl eq ''");
